c8066c14b9e505b8c0195512c33475d86782400f
Rubdh <benrhu@gmail.com>
2022-04-24 18:33:56 +0200

    // Users added

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
+#![allow(non_snake_case)]
 mod coche;
-use coche::Coche;
 use coche::Transmission;
 use coche::fabrica;
 
 fn main() {
-    let mut ferrari = fabrica(String::from("Red"), Transmission::Manual, false);
+    let ferrari = fabrica(String::from("Red"), Transmission::Automatic, false);
     println!("Car 1 = {}, {:?} transmission, convertible: {}, mileage: {}", ferrari.color, ferrari.transmission, ferrari.convertible, ferrari.mileage);
 }
---
+++ src/users.rs
@@ -0,0 +1,1 @@
+let users = ["Rubén", "Esther", "Carlos", "Álex"];
